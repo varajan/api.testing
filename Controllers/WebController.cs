a5feef4e47110f922bf86a35905acdcb6dcf78bf
Sergii Dmytrenko <SEDO@simcorp.com>
2021-03-20 16:19:13 +0200

    Add blank page

---
+++ Controllers/WebController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.testing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Index()
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = System.IO.File.ReadAllText("HTML/page.html")
+            };
+        }
+    }
+}
