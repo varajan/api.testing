475daad1bb7d660a03f51350d12920f4e39d9989
Sergii Dmytrenko <SEDO@simcorp.com>
2021-03-20 20:49:40 +0200

    add swagger

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace api.testing
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +29,12 @@ namespace api.testing
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contacts API");
+            });
 
             app.UseRouting();
 
